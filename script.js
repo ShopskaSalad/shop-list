84ab90054686868ce82a535a7f986c5c085f45ae
ShopskaSalad <akinkamilov@gmail.com>
2024-07-27 12:43:39 +0300

    FEAT / edit mode improvements

--- script.js
+++ script.js
@@ -23,6 +23,16 @@ function onAddItemSubmit(e) {
         return;
     }
 
+    // Check for edit mode
+    if(isEditMode) {
+        const itemToEdit = itemList.querySelector('.edit-mode')
+
+        removeItemFromStorage(itemToEdit.textContent);
+        itemToEdit.classList.remove('edit-mode');
+        itemToEdit.remove();
+        isEditMode = false;
+    }
+
     // Create item DOM element
     addItemToDOM(newItem);
 
@@ -161,6 +171,8 @@ function filterItems(e) {
 
 // clear UI
 function checkUI() {
+
+ itemInput.value = '';
  const items = itemList.querySelectorAll('li');
  if(items.length === 0) {
     clearBtn.style.display = 'none';
@@ -169,6 +181,11 @@ function checkUI() {
     clearBtn.style.display = 'block';
     itemFilter.style.display = 'block';
  }
+
+ formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+ formBtn.style.backgroundColor = '#333'
+
+ isEditMode = false;
 }
 
 // Initiliaze app
